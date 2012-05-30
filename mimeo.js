002c14691ce149c499ab7d267ea11aa3dd598556
Sandro Pasquali <spasquali@gmail.com>
2012-05-30 15:40:41 -0400

    buildout and example :)

--- mimeo.js
+++ mimeo.js
@@ -1,11 +1,182 @@
 (function($) {
 
-	var _data 	= $.fn._data;
-	var slice	= Array.prototype.slice
+	//////////////////////////////////////////////////////////////////////////////////////////
+	//																						//
+	//					Begin Template engine. You can easily swap it.						//
+	//																						//
+	//////////////////////////////////////////////////////////////////////////////////////////
 
-	$.fn._data = function() {
-		console.log(arguments);
-		_data.apply($, slice.call(arguments));
+	// doT.js
+	// 2011, Laura Doktorova, https://github.com/olado/doT
+	//
+	// doT.js is an open source component of http://bebedo.com
+	// Licensed under the MIT license.
+	//
+	var doT = {
+		version: '0.2.0',
+		templateSettings: {
+			evaluate:    /\{\{([\s\S]+?)\}\}/g,
+			interpolate: /\{\{=([\s\S]+?)\}\}/g,
+			encode:      /\{\{!([\s\S]+?)\}\}/g,
+			use:         /\{\{#([\s\S]+?)\}\}/g,
+			define:      /\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g,
+			conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
+			iterate:     /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
+			varname: 'binding',
+			strip: true,
+			append: true,
+			selfcontained: false
+		},
+		template: undefined, //fn, compile template
+		compile:  undefined  //fn, for express
+	};
+
+	var global = (function(){ return this || (0,eval)('this'); }());
+
+	//	spasquali@gmail.com : We don't want (need) these in the global.
+	//
+	if (typeof module !== 'undefined' && module.exports) {
+		//module.exports = doT;
+	} else if (typeof define === 'function' && define.amd) {
+		//define(function(){return doT;});
+	} else {
+		//global.doT = doT;
 	}
+
+	function encodeHTMLSource() {
+		var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': '&#34;', "'": '&#39;', "/": '&#47;' },
+			matchHTML = /&(?!\\w+;)|<|>|"|'|\//g;
+		return function(code) {
+			return code ? code.toString().replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : code;
+		};
+	}
+	global.encodeHTML = encodeHTMLSource();
+
+	var startend = {
+		append: { start: "'+(",      end: ")+'",      startencode: "'+encodeHTML(" },
+		split:  { start: "';out+=(", end: ");out+='", startencode: "';out+=encodeHTML("}
+	}, skip = /$^/;
+
+	function resolveDefs(c, block, def) {
+		return ((typeof block === 'string') ? block : block.toString())
+		.replace(c.define || skip, function(m, code, assign, value) {
+			if (code.indexOf('def.') === 0) {
+				code = code.substring(4);
+			}
+			if (!(code in def)) {
+				if (assign === ':') {
+					def[code]= value;
+				} else {
+					eval("def['"+code+"']=" + value);
+				}
+			}
+			return '';
+		})
+		.replace(c.use || skip, function(m, code) {
+			var v = eval(code);
+			return v ? resolveDefs(c, v, def) : v;
+		});
+	}
+
+	function unescape(code) {
+		return code.replace(/\\('|\\)/g, "$1").replace(/[\r\t\n]/g, ' ');
+	}
+
+	doT.template = function(tmpl, c, def) {
+		c = c || doT.templateSettings;
+		var cse = c.append ? startend.append : startend.split, str, needhtmlencode, sid=0, indv;
+
+		if (c.use || c.define) {
+			var olddef = global.def; global.def = def || {}; // workaround minifiers
+			str = resolveDefs(c, tmpl, global.def);
+			global.def = olddef;
+		} else str = tmpl;
+
+		str = ("var out='" + (c.strip ? str.replace(/(^|\r|\n)\t* +| +\t*(\r|\n|$)/g,' ')
+					.replace(/\r|\n|\t|\/\*[\s\S]*?\*\//g,''): str)
+			.replace(/'|\\/g, '\\$&')
+			.replace(c.interpolate || skip, function(m, code) {
+				return cse.start + unescape(code) + cse.end;
+			})
+			.replace(c.encode || skip, function(m, code) {
+				needhtmlencode = true;
+				return cse.startencode + unescape(code) + cse.end;
+			})
+			.replace(c.conditional || skip, function(m, elsecase, code) {
+				return elsecase ?
+					(code ? "';}else if(" + unescape(code) + "){out+='" : "';}else{out+='") :
+					(code ? "';if(" + unescape(code) + "){out+='" : "';}out+='");
+			})
+			.replace(c.iterate || skip, function(m, iterate, vname, iname) {
+				if (!iterate) return "';} } out+='";
+				sid+=1; indv=iname || "i"+sid; iterate=unescape(iterate);
+				return "';var arr"+sid+"="+iterate+";if(arr"+sid+"){var "+vname+","+indv+"=-1,l"+sid+"=arr"+sid+".length-1;while("+indv+"<l"+sid+"){"
+					+vname+"=arr"+sid+"["+indv+"+=1];out+='";
+			})
+			.replace(c.evaluate || skip, function(m, code) {
+				return "';" + unescape(code) + "out+='";
+			})
+			+ "';return out;")
+			.replace(/\n/g, '\\n').replace(/\t/g, '\\t').replace(/\r/g, '\\r')
+			.replace(/(\s|;|}|^|{)out\+='';/g, '$1').replace(/\+''/g, '')
+			.replace(/(\s|;|}|^|{)out\+=''\+/g,'$1out+=');
+
+		if (needhtmlencode && c.selfcontained) {
+			str = "var encodeHTML=(" + encodeHTMLSource.toString() + "());" + str;
+		}
+		try {
+			return new Function(c.varname, str);
+		} catch (e) {
+			if (typeof console !== 'undefined') console.log("Could not create a template function: " + str);
+			throw e;
+		}
+	};
+
+	doT.compile = function(tmpl, def) {
+		return doT.template(tmpl, null, def);
+	};
+
+	//////////////////////////////////////////////////////////////////////////////////////////
+	//																						//
+	//									End Template engine.								//
+	//																						//
+	//////////////////////////////////////////////////////////////////////////////////////////
+
+
+	//	Override jQuery#data,#removeData methods, watching for changes (two arguments, remove),
+	//	updating template if changes occur. No
+	//
+	//	Normal #data behavior persists, with no modification, first.
+	//
+	//	Errors should be noisy if you don't properly bind a template, etc.
+	//
+	var _data 		= $.fn.data;
+	var _removeData	= $.fn.removeData;
+
+	var update	= function(args, _this, force) {
+
+		var $target	= $(_this);
+
+		if(force || args.length === 2 || $.isPlainObject(args[0])) {
+			var boundTemplateId = $target.attr("data-template");
+			if(boundTemplateId) {
+				$target.html(doT.template($("#" + boundTemplateId).text())($target.data()));
+			}
+		}
+	};
+
+	$.fn.data = function() {
+		var methRes	= _data.apply(this, arguments);
+		update(arguments, this);
+		return methRes;
+	};
+
+	$.fn.removeData = function() {
+		var methRes	= _removeData.apply(this, arguments);
+		update(arguments, this, 1);
+		return methRes;
+	};
+
+	return $;
 
 })(jQuery);
